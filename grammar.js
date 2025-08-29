e4540c7ac05cb4a9c007240e8303eea63e74d5f2
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-08-29 12:06:50 -0400

    testing things

--- grammar.js
+++ grammar.js
@@ -1,5 +1,5 @@
 /**
- * @file Quarto 
+ * @file Quarto
  * @author CoasterFan5 <CoasterFan9@gmail.com>
  * @license MIT
  */
@@ -11,7 +11,24 @@ module.exports = grammar({
   name: "quarto",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._definition),
+
+    _definition: ($) => $.function_definition,
+
+    function_definition: ($) => seq("func", $.identifier, $._type, $.block),
+
+    _type: ($) => choice("bool", "string"),
+
+    block: ($) => seq("{", repeat($._statement), "}"),
+
+    _statement: ($) => $.return_statement,
+
+    return_statement: ($) => seq("return", $._expression, ";"),
+
+    _expression: ($) => choice($.identifier, $.number),
+
+    identifier: ($) => /[a-z]+/,
+
+    number: ($) => /\d+/,
+  },
 });
